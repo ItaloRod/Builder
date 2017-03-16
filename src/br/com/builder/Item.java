27068b227c141378fd38e7e307d0cdaab89295d4
Paulo <Pauloitalo@hotmail.com.br>
2017-03-16 19:32:57 -0300

    Builder

---
+++ src/br/com/builder/Item.java
@@ -0,0 +1,10 @@
+package br.com.builder;
+
+public interface Item {
+	
+	//Metodos de construção de produtos
+	public String name();
+	public Packing packing();
+	public float price();
+
+}
---
+++ src/br/com/builder/Packing.java
@@ -0,0 +1,6 @@
+package br.com.builder;
+
+public interface Packing {
+
+	public String pack();
+}
